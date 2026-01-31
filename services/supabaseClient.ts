266919af6aa7a384af5a37ac8e6956d41d52b773
Bakhtear00 <63471297+Bakhtear00@users.noreply.github.com>
2026-01-31 22:46:54 +0600

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,11 +1,10 @@
-
-
-
-
 import { createClient } from '@supabase/supabase-js';
 
-// আপনার সুপাবেস ড্যাশবোর্ড থেকে এই দুটি ভ্যালু চেক করে নিন
-const supabaseUrl = 'dtdzotfhixgwqujdhkbv';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0ZHpvdGZoaXhnd3F1amRoa2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNTE0MjMsImV4cCI6MjA4NDcyNzQyM30.R00o2oik8hexR-wPpO0v7v0tef8JWY0QojkZB6Cf1_k';
+const supabaseUrl = 'https://dtdzotfhixgwqujdhkbv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0ZHpvdGZoaXhnd3F1amRoa2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNTE0MjMsImV4cCI6MjA4NDcyNzQyM30.R00o2oik8hexR-wPpO0v7v0tef8JWY0QojkZB6Cf1_k';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be provided.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
