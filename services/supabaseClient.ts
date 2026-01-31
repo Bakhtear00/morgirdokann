6dea8596db8deb9617e08a689cda02ae4f014f1f
Bakhtear00 <63471297+Bakhtear00@users.noreply.github.com>
2026-01-31 20:40:29 +0600

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,11 @@
-import { createClient } from '@supabase/supabase-client';
 
-const supabaseUrl = 'https://dtdzotfhixgwqujdhkbv.supabase.co';
-const supabaseKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0ZHpvdGZoaXhnd3F1amRoa2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNTE0MjMsImV4cCI6MjA4NDcyNzQyM30.R00o2oik8hexR-wPpO0v7v0tef8JWY0QojkZB6Cf1_k';
 
+
+
+import { createClient } from '@supabase/supabase-js';
+
+// আপনার সুপাবেস ড্যাশবোর্ড থেকে এই দুটি ভ্যালু চেক করে নিন
+const supabaseUrl = 'dtdzotfhixgwqujdhkbv';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0ZHpvdGZoaXhnd3F1amRoa2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNTE0MjMsImV4cCI6MjA4NDcyNzQyM30.R00o2oik8hexR-wPpO0v7v0tef8JWY0QojkZB6Cf1_k';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
